00a183986c22e83d01340f56092cd9724c478304
DilpreetSohi <DilpreetSohi@.github.com>
2020-10-15 02:42:37 -0700

    dev2

--- app/src/main/java/ca/bcit/androidassn1/FirstFragment.java
+++ app/src/main/java/ca/bcit/androidassn1/FirstFragment.java
@@ -1,34 +1,82 @@
-package ca.bcit.androidassn1;
-
+package ca.bcit.assn1;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends AppCompatActivity {
+    String search;
+    String date;
 
+
+    ArrayList<JSONObject> articleAr = new ArrayList();
+
+    ArrayList<String> titleAr = new ArrayList();
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
-    }
-
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_first);
+        Intent intent = getIntent();
+        final ListView list = findViewById(R.id.list);
+        search = intent.getStringExtra("user_text");
+        date = intent.getStringExtra("date");
+        Result(list);
+        final Intent intentSecond = new Intent(this, SecondFragment.class);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
+                intentSecond.putExtra("info", articleAr.get(pos).toString());
+                intentSecond.putExtra("search", search);
+                intentSecond.putExtra("dates", date);
+                startActivity(intentSecond);
             }
         });
+
     }
+
+    public void Result(final ListView listView){
+        String loc = "https://newsapi.org/v2/everything?q=" + search +"&from=" + date +
+                "&sortBy=publishedAt&apiKey=b09c82ffb5e04665a865932eb6f61af6";
+        RequestQueue req = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, loc, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject res) {
+                        JSONArray jsonRes = res.optJSONArray("articles");
+                        for (int x = 0; x < jsonRes.length(); x++) {
+                            try {
+                                titleAr.add(jsonRes.getJSONObject(x).getString("title"));
+                                articleAr.add(jsonRes.getJSONObject(x));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        ArrayAdapter<String> arrAdapt = new ArrayAdapter<>(FirstFragment.super.getApplicationContext(),
+                                android.R.layout.simple_list_item_1, titleAr);
+                        listView.setAdapter(arrAdapt);
+                    }
+                }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Log.e("Error occured", "Errors may have been detected");
+            }
+        });
+        req.add(jsonObjReq);
+    }
+
 }
--- app/src/main/java/ca/bcit/androidassn1/SecondFragment.java
+++ app/src/main/java/ca/bcit/androidassn1/SecondFragment.java
@@ -1,34 +1,49 @@
-package ca.bcit.androidassn1;
-
+package ca.bcit.assn1;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class SecondFragment extends Fragment {
+import com.squareup.picasso.Picasso;
 
-    @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SecondFragment extends AppCompatActivity {
+
+    String dates, search;
+    TextView link, title, publishDate, descr, info, src;
+    ImageView imageLink;
+    public void onCreate(Bundle savedInstanceData) {
+
+        super.onCreate(savedInstanceData);
+        setContentView(R.layout.fragment_second);
+        imageLink = findViewById(R.id.imgLink);
+        publishDate = findViewById(R.id.publishDate);
+        info = findViewById(R.id.info);
+        link = findViewById(R.id.link);
+        title = findViewById(R.id.title);
+        src = findViewById(R.id.src);
+        descr = findViewById(R.id.descr);
+        Intent intent = getIntent();
+        dates = intent.getStringExtra("dates");
+        search = intent.getStringExtra("search");
+        try{
+            JSONObject json = new JSONObject(intent.getStringExtra("info"));
+            publishDate.setText(json.getString("publishedAt"));
+            link.setText(json.getString("url"));
+            src.setText(json.getJSONObject("source").getString("name"));
+            descr.setText(json.getString("description"));
+            title.setText(json.getString("title"));
+            info.setText(json.getString("content"));
+            String imgLoc = json.getString(("urlToImage"));
+            Picasso.get().load(imgLoc).into(imageLink);
+        }catch(JSONException e){
+            Log.d("Error Occurred", "Error Occurred");
+        }
     }
 
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });
-    }
 }
